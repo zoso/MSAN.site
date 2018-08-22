b6230265281136b8fc94e94c3c2dab517c99c743
Lars Lund <larslund@gmail.com>
2018-08-22 14:57:17 +0200

    some api stuff

--- Scripts/api/ajax_requests.js
+++ Scripts/api/ajax_requests.js
@@ -1,8 +1,31 @@
 //https://kapeli.com/cheat_sheets/Axios.docset/Contents/Resources/Documents/index
 //http://localhost:3000/abonnement?_embed=customers
+//http://ngasweb1t/api/konsept/Mat_For_Minsten/abonnement
 import axios from 'axios';
+import faker from 'faker';
+faker.locale = "nb_NO";
+
 const proxy = "https://cors-anywhere.herokuapp.com";
 const base = "http://localhost:3000";
+
+const createPerson = () => {
+    const obj = {
+        "Fornavn": faker.name.firstName(),
+        "Etternavn": faker.name.lastName(),
+        "Adresse": faker.address.secondaryAddress(),
+        "Postnummer": faker.address.zipCode(),
+        "Poststed": faker.address.city(),
+        "Mobilnummer": faker.phone.phoneNumber(),
+        "Telefonnummer": faker.phone.phoneNumber(),
+        "Epost": faker.internet.email(),
+        "Dagligleder": faker.random.boolean(),
+        "Innkjøpsansvarlig": faker.random.boolean(),
+        "Økonomiansvarlig": faker.random.boolean(),
+        "Stilling": faker.name.jobDescriptor()
+    };
+    return obj;
+}
+
 export default {
     getAllCustomers() {
         return axios.get(`${base}/customers`)
@@ -76,8 +99,52 @@ export default {
             console.log("update contactperson", err);
             return false;
         })
+    },
+    testGetAbo() {
+        return axios.get('http://ngasweb1t.norgesgruppen.no/api/konsept/Mat_For_Minsten/abonnement')
+            .then(res => {
+                return res;
+            })
+            .catch(err => {
+                console.log("err", err);
+            })
+    },
+    createContactPerson() {
+        var person = createPerson();
+        console.log("createPerson", person);
+        return axios.post('http://ngasweb1t.norgesgruppen.no/api/kundekontakt', {
+            person
+        })
+        .then(res => {
+            return res;
+        })
+        .catch(err => {
+            return err;
+        })
+    },
+    getContactPerson() {
+        return createPerson();
     }
 }
+
+/*
+{
+  "Id": 0,
+  "Fornavn": "string",
+  "Etternavn": "string",
+  "Adresse": "string",
+  "Postnummer": "string",
+  "Poststed": "string",
+  "Mobilnummer": "string",
+  "Telefonnummer": "string",
+  "Epost": "string",
+  "Dagligleder": true,
+  "Innkjøpsansvarlig": true,
+  "Økonomiansvarlig": true,
+  "Stilling": "string"
+}
+*/
+
 /*
 curl -i -X OPTIONS -H "Origin: http://127.0.0.1:1337" \
     -H 'Access-Control-Request-Method: GET' \
